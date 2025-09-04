c97c9f67df7bd5b18aaff395de65113f0cef0276
KTG084 <karanjyotimedhi5678@gmail.com>
2025-09-05 03:52:08 +0530

    yh

--- app/rank/page.tsx
+++ app/rank/page.tsx
@@ -3,6 +3,6 @@ import { fetchandCache } from "./fetchRank";
 
 export default async function Rank() {
   const resumes = await fetchandCache("OK");
-
+  
   return <RankLeaderboard resumes={resumes} />;
 }
